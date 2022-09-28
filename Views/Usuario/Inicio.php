8276dab2d2f80c205b7f0cc50fdc465984359130
builes2 <sebasbuile23@gmail.com>
2022-09-28 15:22:33 -0500

    Hola

---
+++ Views/Usuario/Inicio.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once 'C:\xampp\htdocs\Marlly MVC\Inc\Header.php'
+
+?>
+
+<script>
+    alert("Bievenido al sistema <?php echo $_SESSION['nombre'];?>")
+</script>
+
+<?php
+
+include_once 'C:\xampp\htdocs\Marlly MVC\Inc\Footer.php'
+
+?>
